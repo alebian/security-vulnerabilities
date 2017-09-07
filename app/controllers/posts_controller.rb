247ef56defad838e52b193a1b08bf77d8e0a9f95
ilanusse <inakilanusse@gmail.com>
2017-09-07 15:59:35 -0300

    Basic SQL injection example

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  # Remember to check out: https://rails-sqli.org/
+  # And: http://guides.rubyonrails.org/security.html
+
+  # For node & postgresql: https://github.com/brianc/node-postgres/wiki/FAQ#8-does-node-postgres-handle-sql-injection
+
+  def show
+    # Compare http://localhost:3000/posts/1
+    # with http://localhost:3000/posts/1')%20OR%20('1'%20=%20'1
+    @post = Post.where("id = '#{params[:id]}'")
+  end
+end
